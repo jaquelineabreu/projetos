7b6ad9198963ca5232620ac8384fa7e2a2b1fd07
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2022-12-20 23:45:43 -0300

    variaveis em go

--- go/main.go
+++ go/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main(){
-	fmt.Println("Olá, Mundo!")
+	// var nome string = "Jaque"
+	// var idade int = 24
+	// var nome = "Jaque"
+	// var idade  = 24
+	// var versao = 1.1
+	nome := "Jaque"
+	idade  := 24
+	versao := 1.1
+	fmt.Println("Olá, sra", nome, "sua idade é", idade)
+	fmt.Println("Este programa esta na versão", versao)
+
+	fmt.Println("O tipo da variavel versao é", reflect.TypeOf(versao))
 }
