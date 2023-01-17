11e792ef7eb6a68b2882b1b5c51e6e94f1b6a396
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-17 12:09:10 -0300

    refatoração com funções

--- go/main.go
+++ go/main.go
@@ -2,35 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
+	
+	exibeIntroducao()	
+	exibeMenu()
+	comando := leComando()
+	 
+	switch comando {
+		case 1:
+			fmt.Println("Monitorando...")	
+		case 2:
+			fmt.Println("Exibindo logs...")	
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)		
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+	}
+
+	
+	
+}
+
+func exibeIntroducao(){
 	nome := "Jaque"
 	versao := 1.1
 	fmt.Println("Olá, sra", nome)
 	fmt.Println("Este programa esta na versão", versao)
+}
+
+func exibeMenu(){
 	fmt.Println("-----------------------------------")
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
+func leComando()int {	
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
 
-	fmt.Println("O comando escolhido foi", comando)
-
-	switch comando {
-		case 1:
-			fmt.Println("Monitorando...")	
-
-		case 2:
-			fmt.Println("Exibindo logs...")	
-
-		case 0:
-			fmt.Println("Saindo do programa")
-		
-		default:
-			fmt.Println("Não conheço este comando")
-	}
-	
+	return comandoLido
 }
