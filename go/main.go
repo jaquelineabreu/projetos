d131e21ea77ab0eb41af60816c6b080860eed8ab
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-17 11:13:41 -0300

    aula 3 - controle de fluxo if e convensões

--- go/main.go
+++ go/main.go
@@ -20,11 +20,11 @@ func main(){
 	fmt.Println("O comando escolhido foi", comando)
 
 	if comando == 1 {
-		
+		fmt.Println("Monitorando...")		
 	}else if comando == 2 {
-		
+		fmt.Println("Exibindo logs...")				
 	}else if comando == 0 {
-		
+		fmt.Println("Saindo do programa")
 	}else{
 		fmt.Println("Não conheço este comando")
 	}
