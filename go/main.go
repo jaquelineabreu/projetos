27dd1a795db14ec10364b33d2f9f8b90eaac4634
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-17 11:27:11 -0300

    controle de fluxo com switch

--- go/main.go
+++ go/main.go
@@ -19,14 +19,18 @@ func main(){
 
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
-		fmt.Println("Monitorando...")		
-	}else if comando == 2 {
-		fmt.Println("Exibindo logs...")				
-	}else if comando == 0 {
-		fmt.Println("Saindo do programa")
-	}else{
-		fmt.Println("Não conheço este comando")
-	}
+	switch comando {
+		case 1:
+			fmt.Println("Monitorando...")	
 
+		case 2:
+			fmt.Println("Exibindo logs...")	
+
+		case 0:
+			fmt.Println("Saindo do programa")
+		
+		default:
+			fmt.Println("Não conheço este comando")
+	}
+	
 }
