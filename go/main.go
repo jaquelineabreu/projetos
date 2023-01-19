d17e4b868b5a8eea596c06ad2328728da6d94525
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-01-19 17:22:31 -0300

    array e slice

--- go/main.go
+++ go/main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"reflect"
 )
 
 func main(){
-	
-	exibeIntroducao()	
+	exibeNomes()
+	//exibeIntroducao()	
 
 	for{
-		exibeMenu()
+//exibeMenu()
 		comando := leComando()
 		
 		switch comando {
@@ -56,9 +57,17 @@ func leComando()int {
 
 func iniciarMonitoramento(){
 	fmt.Println("Monitorando...")	
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://www.alura.com.br"
+	sites[2] = "https://www.caelum.com.br"
+
+
+
+
+	fmt.Println(sites)
 
 	site := "https://random-status-code.herokuapp.com/"
-
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
@@ -69,3 +78,18 @@ func iniciarMonitoramento(){
 
 }
 
+
+func exibeNomes(){
+	nomes := []string{"Douglas", "Daniel", "Bernardo"}	
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("O meu do slice tem", len(nomes), "itens")
+	fmt.Println("O meu do slice tem capacidade para", cap(nomes), "itens")
+
+	nomes = append(nomes, "Aparecida")
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("O meu do slice tem", len(nomes), "itens")
+	fmt.Println("O meu do slice tem capacidade para", cap(nomes), "itens")
+
+}
